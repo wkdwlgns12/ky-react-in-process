e1904ce7342b7673cf4be2ade19b207c1e1d583b
wkdwlgns12 <jjg530@kbu.ac.kr>
2025-08-06 12:14:22 +0900

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import './App.css'
-import Section from './components/Section'
+import Button from './components/Button'
 
 function App() {
-  
-
   return (
     <div>
-      <Section/>
+      
       <h1>hello React!</h1>
+      <Button text={'메일'} />
+      <Button text={'카페'} />
+      <Button text={'블로그'} />
     </div>
   )
 }
 
-export default App
+export default App;

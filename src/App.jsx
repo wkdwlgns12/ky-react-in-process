ef892302fffd0209436d5ab43fb021e67c89fc90
wkdwlgns12 <jjg530@kbu.ac.kr>
2025-08-06 12:02:00 +0900

    init

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Section from './components/Section'
+
+function App() {
+  
+
+  return (
+    <div>
+      <Section/>
+      <h1>hello React!</h1>
+    </div>
+  )
+}
+
+export default App

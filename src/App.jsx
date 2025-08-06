c192b69af3b04555b529aa26f0d92dbfbb3239e7
wkdwlgns12 <jjg530@kbu.ac.kr>
2025-08-06 13:53:03 +0900

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,13 @@
 
 import './App.css'
-import Button from './components/Button'
-import Input from './components/Input'
+import Counter from './components/Counter'
+
 function App() {
 
 
   return (
     <div>
-      <Input
-        inputValue={'hello react'}
-        title={'input title'}
-        placeholder={'입력하세요'} />
-      <h1>hello React!</h1>
-      <Button text={'메일'} color={'red'} />
-      <Button text={'카페'} color={'blue'} />
-      <Button text={'블로그'} color={'orange'} />
-      <Button text={'기본값 설정'} />
-      <Button text={'자식요소 설정'} color={'pink'}>
-        <strong>중요</strong>
-      </Button>
+      <Counter/>
     </div>
   )
 }
